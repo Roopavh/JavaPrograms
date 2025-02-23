385083ff8528e4ef242288003b0f1097090d9d5f
Roopavh <roopavh.koppal@gmail.com>
2025-02-23 20:42:36 +0530

    Initial commit

---
+++ src/Feb/datatypes/Datatype.java
@@ -0,0 +1,7 @@
+package Feb.datatypes;
+
+public class Datatype {
+    public static void main(String[] args) {
+        System.out.println("Data types and their size and min and max values");
+    }
+}
