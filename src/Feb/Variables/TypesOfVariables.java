97e8f716d0307e704de778e79020e2b2e9ead152
Roopavh <roopavh.koppal@gmail.com>
2025-02-23 22:35:18 +0530

    Variables

---
+++ src/Feb/Variables/TypesOfVariables.java
@@ -0,0 +1,20 @@
+package Feb.Variables;
+
+public class TypesOfVariables {
+    public static void main(String[] args) {
+
+        /*There are three types of variables
+        1. Local Variables
+        2. Static Variables
+        3. Instance Variables
+        Note : There is no concept of global variables in java*/
+
+        System.out.println("There are three types of variables");
+        System.out.println("1. Local Variables");
+        System.out.println("2. Static Variables");
+        System.out.println("3. Instance Variables");
+        System.out.println();
+
+        System.out.println("There is no concept of global variables in java");
+    }
+}
