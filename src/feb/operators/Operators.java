d22522c3873f74334d2b9fb8f71a68376360b47a
Roopavh <roopavh.koppal@gmail.com>
2025-02-23 23:47:43 +0530

    Operators

--- src/feb/operators/Operators.java
+++ src/feb/operators/Operators.java
@@ -3,10 +3,7 @@ package feb.operators;
 public class Operators {
     public static void main(String[] args) {
         System.out.println("Operators in java");
-        System.out.println("Operators in java");
-        System.out.println("Operators in java");
-        System.out.println("Operators in java");
-        System.out.println("Operators in java");
+
 
     }
 }
