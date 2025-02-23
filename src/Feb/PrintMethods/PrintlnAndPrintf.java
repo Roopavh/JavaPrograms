7271584d68049e8b8ac6eebe83e06f98d7206ad7
Roopavh <roopavh.koppal@gmail.com>
2025-02-23 22:32:15 +0530

    Variables

--- src/Feb/PrintMethods/PrintlnAndPrintf.java
+++ src/Feb/PrintMethods/PrintlnAndPrintf.java
@@ -8,15 +8,15 @@ public class PrintlnAndPrintf {
         int table = 12;
         System.out.println("Printing tables using printf() method");
         System.out.printf("%d * %d = %d",table,1,table*1);System.out.println();
-        System.out.printf("%d * %d = %d",table,1,table*2);System.out.println();
-        System.out.printf("%d * %d = %d",table,1,table*3);System.out.println();
-        System.out.printf("%d * %d = %d",table,1,table*4);System.out.println();
-        System.out.printf("%d * %d = %d",table,1,table*5);System.out.println();
-        System.out.printf("%d * %d = %d",table,1,table*6);System.out.println();
-        System.out.printf("%d * %d = %d",table,1,table*7);System.out.println();
-        System.out.printf("%d * %d = %d",table,1,table*8);System.out.println();
-        System.out.printf("%d * %d = %d",table,1,table*9);System.out.println();
-        System.out.printf("%d * %d = %d",table,1,table*10);System.out.println();
+        System.out.printf("%d * %d = %d",table,2,table*2);System.out.println();
+        System.out.printf("%d * %d = %d",table,3,table*3);System.out.println();
+        System.out.printf("%d * %d = %d",table,4,table*4);System.out.println();
+        System.out.printf("%d * %d = %d",table,5,table*5);System.out.println();
+        System.out.printf("%d * %d = %d",table,6,table*6);System.out.println();
+        System.out.printf("%d * %d = %d",table,7,table*7);System.out.println();
+        System.out.printf("%d * %d = %d",table,8,table*8);System.out.println();
+        System.out.printf("%d * %d = %d",table,9,table*9);System.out.println();
+        System.out.printf("%d * %d = %d",table,10,table*10);System.out.println();
 
     }
 }
