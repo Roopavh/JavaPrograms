095953aff431947c499c7cdda0a468937b7bf0e3
Roopavh <roopavh.koppal@gmail.com>
2025-02-25 17:57:54 +0530

    Printf()

--- src/feb/printMethods/PrintlnAndPrintf.java
+++ src/feb/printMethods/PrintlnAndPrintf.java
@@ -3,11 +3,22 @@ package feb.printMethods;
 public class PrintlnAndPrintf {
     public static void main(String[] args) {
 
-        //Printing tables using printf() method
+/*
+        Printing tables using printf() method
+
+     printf()- formatting different datatype values
+        %f -> floating, double
+        %d -> int, byte, long, short
+        %s -> String
+        %b -> boolean
+        %x -> Hex
+*/
 
         int table = 12;
-        System.out.println("Printing tables using printf() method");
-        System.out.printf("%d * %d = %d",table,1,table*1);System.out.println();
+        String name= "Roopa";
+
+        System.out.printf("%s is writing tables of %d", name , table);System.out.println();
+        System.out.printf("%d * %d = %d",table,1,table);System.out.println();
         System.out.printf("%d * %d = %d",table,2,table*2);System.out.println();
         System.out.printf("%d * %d = %d",table,3,table*3);System.out.println();
         System.out.printf("%d * %d = %d",table,4,table*4);System.out.println();
