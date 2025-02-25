5996ea86e6186308f56b989b68e5e3b5f926b86c
Roopavh <roopavh.koppal@gmail.com>
2025-02-25 17:42:40 +0530

    Operators

--- src/feb/operators/Operators.java
+++ src/feb/operators/Operators.java
@@ -3,7 +3,21 @@ package feb.operators;
 public class Operators {
     public static void main(String[] args) {
         System.out.println("Operators in java");
+        /*
+        There are three types of operators based number of operands
+          1. Unary operator
+          2. Binary operator
+          3. Ternary operator
 
+          Below are the types of operators based on operation performs on operands
+           1. Assignment operators ->   = always assigns value from RHV to LH
+           2. Arithmetic operators ->   + - / * %  used for basic mathematical operations
+           3. Relational operators ->   < > <= >= == !=    used to compare two more values - Always result ll be true/false
+           4. Logical operators -> && || !
+           5. instanceof
+           6. new
+
+           */
 
     }
 }
