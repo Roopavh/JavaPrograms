dffa87f94bbc3791a56408fba541901c8364a5b7
Roopavh <roopavh.koppal@gmail.com>
2025-02-25 19:07:52 +0530

    Operators

---
+++ src/feb/operators/OperatorOverloading.java
@@ -0,0 +1,16 @@
+package feb.operators;
+
+public class OperatorOverloading {
+    public static void main(String[] args) {
+        //Operator overloading-> + operator behaves differently based on different data types
+        // with numeric datatypes operates as arithmetic operator
+        // with String data type behaves as concatenation
+
+        String name="Roopa";
+        int age= 65;
+
+        System.out.println("Age of " +name+ " after 10 year is " +(age+10));
+
+
+    }
+}
---
+++ src/feb/operators/UnaryOperators.java
@@ -0,0 +1,17 @@
+package feb.operators;
+
+public class UnaryOperators {
+    public static void main(String[] args) {
+        //Unary operators ->operates on single operand
+
+        int a=65; // + -> unary operator
+        int b=-3;  // -unary operator
+
+        System.out.println(a);
+        System.out.println(b);
+
+        b=b+3;
+        System.out.println(b);
+
+    }
+}
